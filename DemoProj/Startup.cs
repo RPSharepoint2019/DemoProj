c1fc3ee8ab3aa4d58fb34aec4298629fa3fc4da8
PIMSPadmin1 <PIMSPadmin1@DEVSHPT201304>
2019-05-06 01:49:04 -0700

    Add project files.

---
+++ DemoProj/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DemoProj.Startup))]
+namespace DemoProj
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
